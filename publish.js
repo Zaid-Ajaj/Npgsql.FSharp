aad1c9bc065f480779b65ed76ca900b6d2b4c5ea
Zaid <zaid.naom@gmail.com>
2021-03-14 02:06:46 +0100

    Updates

---
+++ publish.js
@@ -0,0 +1,15 @@
+var ghPages = require("gh-pages");
+
+var packageUrl = "https://github.com/Zaid-Ajaj/Npgsql.FSharp.git";
+
+console.log("Publishing to ", packageUrl);
+
+ghPages.publish(".", {
+    repo: packageUrl
+}, function (e) {
+    if (e === undefined) {
+        console.log("Finished publishing succesfully");
+    } else {
+        console.log("Error occured while publishing :(", e);
+    }
+});
